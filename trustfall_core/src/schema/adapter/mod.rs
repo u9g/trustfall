d86e41f8ba3621c3a14fcaaa01f141a58617d488
u9g <git@u9g.dev>
2023-07-12 23:27:25 -0400

    optimization api abstraction

--- trustfall_core/src/schema/adapter/mod.rs
+++ trustfall_core/src/schema/adapter/mod.rs
@@ -208,6 +208,36 @@ impl<'a> EdgeParameter<'a> {
     }
 }
 
+fn edge<'a, F, G, A: 'a>(
+    candidate_value: Option<CandidateValue<&FieldValue>>,
+    specific: F,
+    generic: G,
+) -> VertexIterator<'a, A>
+where
+    F: Fn(FieldValue) -> Option<A> + 'a,
+    G: Fn() -> VertexIterator<'a, A>,
+{
+    if let Some(candidate_value) = candidate_value {
+        match candidate_value {
+            CandidateValue::Range(_) | CandidateValue::All => generic(),
+            CandidateValue::Impossible => Box::new(std::iter::empty()),
+            CandidateValue::Single(value) => {
+                Box::new(std::iter::once(specific(value.to_owned())).flatten())
+            }
+            CandidateValue::Multiple(values) => Box::new(
+                values
+                    .iter()
+                    .map(|v| v.to_owned().to_owned()) // I really don't like the .to_owned().to_owned() here...
+                    .collect::<Vec<FieldValue>>()
+                    .into_iter()
+                    .filter_map(specific),
+            ),
+        }
+    } else {
+        generic()
+    }
+}
+
 impl<'a> crate::interpreter::Adapter<'a> for SchemaAdapter<'a> {
     type Vertex = SchemaVertex<'a>;
 
@@ -217,60 +247,29 @@ impl<'a> crate::interpreter::Adapter<'a> for SchemaAdapter<'a> {
         _parameters: &EdgeParameters,
         resolve_info: &ResolveInfo,
     ) -> VertexIterator<'a, Self::Vertex> {
-        let candidate_value = resolve_info.statically_required_property("name").clone();
         match edge_name.as_ref() {
             "VertexType" => {
                 let root_query_type = self.schema.query_type_name();
 
-                if let Some(CandidateValue::Single(FieldValue::String(name_wanted))) =
-                    candidate_value
-                {
-                    let name_wanted = name_wanted.as_str();
-                    if let Some(exact_wanted) = self
-                        .schema
-                        .vertex_types
-                        .get(name_wanted)
-                        .filter(move |v| v.name.node != root_query_type)
-                    {
-                        Box::new(std::iter::once(SchemaVertex::VertexType(VertexType::new(
-                            exact_wanted,
-                        ))))
-                    } else {
-                        Box::new(std::iter::empty())
-                    }
-                } else if let Some(CandidateValue::Multiple(possibilities)) = candidate_value {
-                    let possibilities_as_owned_strings = possibilities
-                        .iter()
-                        .map(|el| {
-                            el.as_str()
-                                .expect("for name possibility to be a string")
-                                .to_string()
-                        })
-                        .collect::<Vec<_>>();
+                let vtypes = &self.schema.vertex_types;
 
-                    let vertex_types = &self.schema.vertex_types;
-
-                    Box::new(
-                        possibilities_as_owned_strings
-                            .into_iter()
-                            .filter_map(move |wanted| {
-                                vertex_types.get(wanted.as_str()).and_then(|exact_wanted| {
-                                    if exact_wanted.name.node != root_query_type {
-                                        Some(SchemaVertex::VertexType(VertexType::new(
-                                            exact_wanted,
-                                        )))
-                                    } else {
-                                        None
-                                    }
-                                })
-                            }),
-                    )
-                } else {
-                    Box::new(self.schema.vertex_types.values().filter_map(move |v| {
-                        (v.name.node != root_query_type)
-                            .then(|| SchemaVertex::VertexType(VertexType::new(v)))
-                    }))
-                }
+                edge(
+                    resolve_info.statically_required_property("name"),
+                    move |f| {
+                        vtypes
+                            .get(f.as_str().unwrap())
+                            .filter(move |v| v.name.node != root_query_type)
+                            .map(|exact_wanted| {
+                                SchemaVertex::VertexType(VertexType::new(exact_wanted))
+                            })
+                    },
+                    || {
+                        Box::new(vtypes.values().filter_map(move |v| {
+                            (v.name.node != root_query_type)
+                                .then(|| SchemaVertex::VertexType(VertexType::new(v)))
+                        }))
+                    },
+                )
             }
             "Entrypoint" => Box::new(Box::new(
                 self.schema
